984d4a5660686e9831e2194dd4cb734a8f2605af
Jacqueline Lee <jmasonlee@gmail.com>
2021-02-05 11:25:41 -0700

    Attempt number 5

--- king_in_check.test.js
+++ king_in_check.test.js
@@ -1,67 +1,19 @@
-import cases from "jest-in-case";
-import kingInCheck from "./king_in_check";
+import cases from 'jest-in-case'
+import createBoard from './createBoard'
+import kingInCheck from './king_in_check'
 
-const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
-
-function getPieceRank(piece) {
-  return piece.rank - 1;
-}
-
-function getPieceFile(piece) {
-  return files.indexOf(piece.file);
-}
-
-const createBoard = (pieces = []) => {
-  const file = () => Array(8).fill("");
-  const board = Array(8).fill(file());
-
-  pieces.forEach((piece) => {
-    const rank = getPieceRank(piece);
-    const file = getPieceFile(piece);
-    board[rank][file] = piece.type;
-  });
-
-  return board;
-};
-
-describe("createBoard", () => {
-  it("should create an empty board when no pieces are provided", () => {
-    const providedValue = JSON.stringify(createBoard().flat());
-    const expectedValue = JSON.stringify(
-      Array(8).fill(Array(8).fill("")).flat()
-    );
-    expect(providedValue).toEqual(expectedValue);
-  });
-  it("should create a board with the provided pieces at the correct location", () => {
-    const piece = {
-      type: "K",
-      rank: "4",
-      file: "a",
-    };
-
-    const board = createBoard([piece]);
-
-    expect(board).toMatchSnapshot()
-  });
-});
-
-
-cases(
-  "kingInCheck",
-  (args) => {
-    const board = createBoard(args.pieces);
-    expect(kingInCheck(board)).toEqual(args.result);
-  },
-  [
+cases('king in check', args => {
+    const board = createBoard(args.pieces)
+    expect(kingInCheck(board)).toEqual(args.expectedResult)
+}, [
     {
-      name: "should return false when board is empty",
-      pieces: [],
-      result: false,
+        name: 'it should return false when board is empty', 
+        pieces: [],
+        expectedResult: false
     },
     {
-      name: 'should return true when king is in check by queen',
-      pieces: [{type: 'K', rank: '4', file: 'b'}, {type: 'Q', rank: '4', file: 'c'}],
-      result: true
+      name: 'it should return true when king is in check by queen',
+      pieces: [{type: 'K', rank: 3, file: 'b'}, {type: 'Q', rank: 3, file: 'c'}],
+      expectedResult: true
     }
-  ]
-);
+])
