26730ca4fd0daa1f9ef13e40e582b779893c464f
Jacqueline Lee <jmasonlee@gmail.com>
2021-02-09 12:04:16 -0700

    Attempt #7

--- king_in_check.js
+++ king_in_check.js
@@ -1,1 +1,18 @@
-export default () => { return false }
+const pieces = {
+    KING: 'K',
+    QUEEN: 'Q',
+    ROOK: 'R'
+}
+
+function findKingFile (board) {
+    return board.find(f => f.includes(pieces.KING));
+}
+
+function isPieceInSameFileAsKing(board) {
+    const kingFile = findKingFile(board)
+    return [pieces.QUEEN, pieces.ROOK].some( p => kingFile.indexOf(p) >= 0)
+}
+
+export default board => {
+    return isPieceInSameFileAsKing(board)
+}
--- king_in_check.test.js
+++ king_in_check.test.js
@@ -1,21 +1,39 @@
 import cases from 'jest-in-case'
 import king_in_check from './king_in_check'
 
-const emptyBoard = [
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','','']
+const kingInCheckByQueenInSameFile = [
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', 'K', 'Q', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '']
+]
+
+const kingInCheckByRookInSameFile = [
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', 'K', '', '', '', '', 'R'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '']
 ]
 
 cases('king_in_check', args => {
     expect(king_in_check(args.board)).toEqual(args.expectedResult)
-}, [{
-    name: 'should return false when board is empty',
-    board: emptyBoard,
-    expectedResult: false
-}])
+}, [
+    {
+        name: 'should return true when Queen is in same file',
+        board: kingInCheckByQueenInSameFile,
+        expectedResult: true
+    },
+    {
+        name: 'should return true when Rook is in same file',
+        board: kingInCheckByRookInSameFile,
+        expectedResult: true
+    }
+])
