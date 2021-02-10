10fe44b2baa18231683f0c6106e4679d2ada61b6
Jacqueline Lee <jmasonlee@gmail.com>
2021-02-10 13:51:36 -0700

    Attempt 8

--- king_in_check.js
+++ king_in_check.js
@@ -1,18 +1,34 @@
-const pieces = {
-    KING: 'K',
-    QUEEN: 'Q',
-    ROOK: 'R'
+function getPieceRank(board, piece) {
+    return board.find(r => r.includes(piece));
 }
 
-function findKingFile (board) {
-    return board.find(f => f.includes(pieces.KING));
+function kingIsInSameRankAsAttackingPiece(board) {
+    const kingRank = getPieceRank(board, 'K')
+    return kingRank.includes('Q');
 }
 
-function isPieceInSameFileAsKing(board) {
-    const kingFile = findKingFile(board)
-    return [pieces.QUEEN, pieces.ROOK].some( p => kingFile.indexOf(p) >= 0)
+function getPieceFileIndex(board, piece) {
+    return getPieceRank(board, piece).indexOf(piece);
+}
+
+function kingIsInSameFileAsAttackingPiece(board) {
+    const kingFile = getPieceFileIndex(board, 'K')
+    const queenFile = getPieceFileIndex(board, 'Q')
+    return kingFile == queenFile;
+}
+
+function kingIsOnDiagonalFromAttackingPiece(board) {
+    const kingRankIndex = board.findIndex(r => r.includes('K'))
+    const queenRankIndex = board.findIndex(r => r.includes('Q'))
+    const rankIndexDiff = Math.abs(kingRankIndex - queenRankIndex)
+
+    const kingFileIndex = getPieceFileIndex(board, 'K')
+    const queenFileIndex = getPieceFileIndex(board, 'Q')
+    const fileIndexDiff = Math.abs(kingFileIndex - queenFileIndex)
+
+    return rankIndexDiff === fileIndexDiff;
 }
 
 export default board => {
-    return isPieceInSameFileAsKing(board)
+    return kingIsInSameRankAsAttackingPiece(board) || kingIsInSameFileAsAttackingPiece(board) || kingIsOnDiagonalFromAttackingPiece(board);
 }
--- king_in_check.test.js
+++ king_in_check.test.js
@@ -1,39 +1,65 @@
 import cases from 'jest-in-case'
 import king_in_check from './king_in_check'
 
-const kingInCheckByQueenInSameFile = [
+const kingAndQueenInSameRank = [
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
-    ['', '', 'K', 'Q', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '']
-]
-
-const kingInCheckByRookInSameFile = [
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', 'K', '', '', '', '', 'R'],
+    ['', '', '', 'K', 'Q', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '']
 ]
 
+const queenNotAttacking = [
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', 'Q', '', ''],
+    ['', '', '', 'K', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '']
+]
+
+const kingAndQueenInSameFile = [
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', 'Q', '', '', '', ''],
+    ['', '', '', 'K', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '']
+];
+
+let queenOnDiagonalToKing = [
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', 'K', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', 'Q', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '']
+];
 cases('king_in_check', args => {
-    expect(king_in_check(args.board)).toEqual(args.expectedResult)
-}, [
-    {
-        name: 'should return true when Queen is in same file',
-        board: kingInCheckByQueenInSameFile,
-        expectedResult: true
-    },
-    {
-        name: 'should return true when Rook is in same file',
-        board: kingInCheckByRookInSameFile,
-        expectedResult: true
-    }
-])
+    expect(king_in_check(args.board)).toBe(args.expectedResult)
+}, [{
+    name: 'should return true if queen is in same rank as king',
+    board: kingAndQueenInSameRank,
+    expectedResult: true
+}, {
+    name: 'should return false if queen is on board, but king is not in check',
+    board: queenNotAttacking,
+    expectedResult: false
+}, {
+    name: 'should return true if queen is in same file as king',
+    board: kingAndQueenInSameFile,
+    expectedResult: true
+}, {
+    name: 'should return true if queen can attack from a diagonal',
+    board: queenOnDiagonalToKing,
+    expectedResult: true
+}])
