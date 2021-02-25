05c354c5dba5a2193597b991690e90bba2f7ae38
Jacqueline Lee <jmasonlee@gmail.com>
2021-02-25 15:17:06 -0700

    Attempt #11

--- king_in_check.js
+++ king_in_check.js
@@ -1,37 +1,11 @@
-export default board => {
+export const isInSameRank = (piece1, piece2) => {
+  return piece1.rank === piece2.rank
+}
 
-  const getRankIndex = piece => {
-    return board.findIndex(r => r.includes(piece))
-  }
+export const isInSameFile = (piece1, piece2) => {
+  return piece1.file === piece2.file
+}
 
-  const kingIsInSameRankAsAttacker = () => {
-    const kingRankIndex = getRankIndex('K')
-    const attackerRankIndex = getRankIndex('Q')
-    return kingRankIndex === attackerRankIndex
-  }
-
-  const getFileIndex = piece => {
-    return board[getRankIndex(piece)].findIndex(f => f === piece)
-  }
-
-  const kingIsInSameFileAsAttacker = () => {
-    const kingFileIndex = getFileIndex('K')
-    const attackerFileIndex = getFileIndex('Q')
-    return kingFileIndex === attackerFileIndex
-  }
-
-  function getIndexDiff(indexFn) {
-    const kingIndex = indexFn('K')
-    const attackerIndex = indexFn('Q')
-    return Math.abs(kingIndex - attackerIndex)
-  }
-
-  const kingIsOnSameDiagonalAsAttacker = () => {
-    const rankIndexDiff = getIndexDiff(getRankIndex)
-    const fileIndexDiff = getIndexDiff(getFileIndex);
-
-    return fileIndexDiff === rankIndexDiff
-  }
-
-  return kingIsInSameRankAsAttacker() || kingIsInSameFileAsAttacker() || kingIsOnSameDiagonalAsAttacker()
+export const isOnSameDiagonal = (piece1, piece2) => {
+   return useSetToDetermineValidityOfDiagonalAttack(Math.abs(piece1.rank - piece2.rank), piece1, piece2)
 }
--- king_in_check.test.js
+++ king_in_check.test.js
@@ -1,73 +1,113 @@
-import cases from 'jest-in-case'
-import king_in_check from './king_in_check'
+import {isInSameFile, isInSameRank, isOnSameDiagonal, pawnCanAttack} from "./king_in_check";
 
+describe('functions', () => {
+  it('should return true if piece is in same rank as king', () => {
+    const king = {
+      type: 'K',
+      rank: 1,
+      file: 4
+    }
+    const piece = {
+      type: 'P',
+      rank: 1,
+      file: 2
+    }
+    expect(isInSameRank(king, piece)).toBe(true)
+  })
 
-let kingInCheckFromQueenInSameRank =
-  [
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', 'K', '', '', 'Q', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '']
-  ]
+  it('should return false if piece is not in same rank as king', function () {
+    const king = {
+      type: 'K',
+      rank: 0,
+      file: 4
+    }
+    const piece = {
+      type: 'P',
+      rank: 1,
+      file: 2
+    }
+    expect(isInSameRank(king, piece)).toBe(false)
+  });
 
-let kingInCheckDiagonally = [
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', 'K', '', '', '', '', ''],
-  ['', '', '', 'Q', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', ''],
-  ['', '', '', '', '', '', '', '']
-]
+  it('should return true if piece is in same file as king', function () {
+    const king = {
+      type: 'K',
+      rank: 0,
+      file: 4
+    }
+    const piece = {
+      type: 'P',
+      rank: 1,
+      file: 4
+    }
+    expect(isInSameFile(king, piece)).toBe(true)
+  });
 
-let kingNotInCheck =
-  [
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', 'K', '', '', '', '', ''],
-    ['', '', '', '', 'Q', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '']
-  ]
-let kingInCheckFromQueenInSameFile =
-  [
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', 'K', '', '', '', '', ''],
-    ['', '', 'Q', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '']
-  ]
-cases('king_in_check', args => {
-  expect(king_in_check(args.board)).toBe(args.expected)
-}, [
-  {
-    name: 'should return true if king is in same rank as queen',
-    board: kingInCheckFromQueenInSameRank,
-    expected: true
-  },
-  {
-    name: 'should return false if king is not in check by queen',
-    board: kingNotInCheck,
-    expected: false
-  },
-  {
-    name: 'should return true if king is in same file as queen',
-    board: kingInCheckFromQueenInSameFile,
-    expected: true
-  },
-  {
-    name: 'should return true in king is in check by queen diagonally',
-    board: kingInCheckDiagonally,
-    expected: true
-  }
-])
+  it('should return false if piece is not in same file as king', function () {
+    const king = {
+      type: 'K',
+      rank: 0,
+      file: 4
+    }
+    const piece = {
+      type: 'P',
+      rank: 0,
+      file: 2
+    }
+    expect(isInSameFile(king, piece)).toBe(false)
+  });
+
+  it('should return true if piece is on same diagonal as king', function () {
+    const king = {
+      type: 'K',
+      rank: 0,
+      file: 0
+    }
+    const piece = {
+      type: 'P',
+      rank: 1,
+      file: 1
+    }
+    expect(isOnSameDiagonal(king, piece)).toBe(true)
+  });
+  it('should return false if piece is not on same diagonal as king', function () {
+    const king = {
+      type: 'K',
+      rank: 3,
+      file: 0
+    }
+    const piece = {
+      type: 'P',
+      rank: 1,
+      file: 1
+    }
+    expect(isOnSameDiagonal(king, piece)).toBe(false)
+  });
+  it('should return true if pawn can attack king', () => {
+    const king = {
+      type: 'K',
+      rank: 4,
+      file: 3
+    }
+    const pawn = {
+      type: 'P',
+      rank: 5,
+      file: 4
+    }
+    expect(pawnCanAttack(pawn, king)).toBe(true)
+  })
+
+  it('should return false if pawn cannot attack king', () => {
+    const king = {
+      type: 'K',
+      rank: 4,
+      file: 3
+    }
+    const pawn = {
+      type: 'P',
+      rank: 3,
+      file: 4
+    }
+    expect(pawnCanAttack(pawn, king)).toBe(false)
+  })
+})
