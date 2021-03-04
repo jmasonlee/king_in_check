67d9444803e8009057f8873e50303a83721145a3
Amaya Navarro <anavarro@atb.com>
2021-03-04 15:35:40 -0700

    isOnSameDiagonal

--- king_in_check.js
+++ king_in_check.js
@@ -1,17 +1,21 @@
 const map = {
- 'K': false,
- 'R': true,
- 'Q': true,
- 'P': false,
- 'B': false,
- 'N': false
+ 'K': () => false,
+ 'R': () => true,
+ 'Q': () => true,
+ 'P': () => false,
+ 'B': isOnSameDiagonal,
+ 'N': () => false
 }
 
-const isOnS
+const isOnSameDiagonal = (p1, p2) =>{
+ let rankDiff = Math.abs(p1.rank - p2.rank)
+ let fileDiff = Math.abs(p1.file - p2.file)
+ return fileDiff === rankDiff
+}
 
 export default board => {
  let piecesOnBoard = board.flat().filter(o => o !== '');
  let result = false
- piecesOnBoard.forEach(piece => { result = result || map[piece]})
+ piecesOnBoard.forEach(piece => { result = result || map[piece]()})
  return result;
 }
