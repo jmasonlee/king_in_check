b03f9e156968f31be08d102643a1ad88c9f8f673
Amaya Navarro <anavarro@atb.com>
2021-03-29 11:35:27 -0600

    code refactor

--- king_in_check.js
+++ king_in_check.js
@@ -1,53 +1,60 @@
-
-
-const checkablePositions = {
-  N: ['Q', 'R'],
-  NW: ['Q', 'B', 'P'],
-  W: ['Q', 'R'],
-  SW: ['Q', 'B'],
-  S: ['Q', 'R'],
-  SE: ['Q', 'B'],
-  E: ['Q', 'R'],
-  NE: ['Q', 'B', 'P'],
-  KN: ['N']
+const canCheckFrom = {
+    STRAIGHT_LINE: ['Q', 'R'],
+    NORTH_DIAGONAL: ['Q', 'B', 'P'],
+    SOUTH_DIAGONAL: ['Q', 'B'],
+    KN: ['N']
+}
+const getFileFromIndex = (index) => {
+    return index % 8
 }
 
+const getRankFromIndex = (index) => {
+    return Math.floor(index / 8)
+}
+
+const getKing = board => {
+  return {
+    index: board.flat().indexOf('K'),
+    file: getFileFromIndex(board.flat().indexOf('K')),
+    rank: getRankFromIndex(board.flat().indexOf('K'))
+  }
+}
 
 const getPiecesFromBoard = board => {
-  const getRankFromIndex = (index) => {
-    return Math.floor(index / 8);
-  }
+  const king = getKing(board)
 
-  const getFileFromIndex = (index) => {
-    return index % 8;
-  }
-
-  const kingIndex = board.flat().indexOf('K')
-  const kingFile = getFileFromIndex(kingIndex)
-  const kingRank = getRankFromIndex(kingIndex)
-
-  return board.flat().map((s, index) => {
-    return s ?
-      {
-        type: s,
-        rank: getRankFromIndex(index) - kingRank,
-        file: getFileFromIndex(index) - kingFile
-      } : ''
-  }).filter(s => s !== '' && s !== 'K')
+    return board.flat().map((s, index) => {
+        return s ?
+            {
+                type: s,
+                rank: getRankFromIndex(index) - king.rank,
+                file: getFileFromIndex(index) - king.file
+            } : ''
+    }).filter(s => s !== '' && s !== 'K')
 }
 
-const canAttackFromNE = piece => {
-  return piece.rank < 0 && piece.file < 0 && piece.file === piece.rank && checkablePositions.NE.includes(piece.type)
+const isOnDiagonalFromKing = piece => piece.file === piece.rank
+
+function isNorthOfKing(piece) {
+    return piece.rank < 0
 }
+
+const canAttackFromNorthDiagonal = piece => {
+    return isNorthOfKing(piece) && isOnDiagonalFromKing(piece) && canCheckFrom.NORTH_DIAGONAL.includes(piece.type)
+}
+
+function isInStraightLineFromKing(piece) {
+    return piece.file === 0
+}
+
 const canAttackFromN = piece => {
-  return piece.rank < 0
-    && piece.file === 0
-    && piece.file !== piece.rank
-    && checkablePositions.N.includes(piece.type)
+    return isNorthOfKing(piece)
+        && isInStraightLineFromKing(piece)
+        && canCheckFrom.STRAIGHT_LINE.includes(piece.type)
 }
 
 export default board => {
-  const pieces = getPiecesFromBoard(board)
+    const pieces = getPiecesFromBoard(board)
 
-  return pieces.length > 1 && pieces.filter(p => canAttackFromNE(p) || canAttackFromN(p)).length > 0
+    return pieces.length > 1 && pieces.filter(p => canAttackFromNorthDiagonal(p) || canAttackFromN(p)).length > 0
 }
