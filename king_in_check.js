12a5032039e56131ba1059b73393e750bfc0c12b
Jacqueline Lee <jmasonlee@gmail.com>
2021-05-05 13:48:06 -0600

    Fresh attempt

--- king_in_check.js
+++ king_in_check.js
@@ -1,78 +1,8 @@
-export default (board) => {
-    const KING = "K"
-    const EMPTY = " "
-
-    board = board.flat()
-    const getFileFromIndex = (index) => {
-        return index % 8
-    }
-
-    const getRankFromIndex = (index) => {
-        return Math.floor(index / 8)
-    }
-
-    const getRelativeLocationToKing = (s, index) => {
-        const kingIndex = board.indexOf(KING)
-        return {
-            type: s,
-            rankDiff: getRankFromIndex(index) - getRankFromIndex(kingIndex),
-            fileDiff: getFileFromIndex(index) - getFileFromIndex(kingIndex)
-        }
-    }
-
-    const getPiecesFromBoard = () => {
-        return board
-            .map((s, index) => getRelativeLocationToKing(s, index))
-    }
-
-    const isInStraightLine = (piece) =>
-        piece.fileDiff === 0 || piece.rankDiff === 0
-
-    const isOnDiagonal = (piece) =>
-        Math.abs(piece.fileDiff) === Math.abs(piece.rankDiff)
-
-    const canPawnAttack = (piece) => {
-        const isNextToKing =
-            Math.abs(piece.rankDiff) === 1 && Math.abs(piece.fileDiff) === 1
-        const isNorthOfKing = piece.rankDiff < 0
-        return isNextToKing && isNorthOfKing
-    }
-
-    const canKnightAttack = (piece) => {
-        const set = new Set([Math.abs(piece.rankDiff), Math.abs(piece.fileDiff)])
-        return set.has(1) && set.has(2)
-    }
-
-    const findBlockingPieces = (isOnVector, piece) => {
-        return pieces.filter(
-            (p) =>
-                isOnVector(p) &&
-                p.type !== EMPTY &&
-                Math.sign(p.rankDiff) === Math.sign(piece.rankDiff) &&
-                Math.sign(p.fileDiff) === Math.sign(piece.fileDiff) &&
-                (Math.abs(p.fileDiff) < Math.abs(piece.fileDiff) ||
-                    Math.abs(p.rankDiff) < Math.abs(piece.rankDiff))
-        )
-    }
-
-    const canAttack = (piece, isOnVector) => {
-        const blockingPieces = findBlockingPieces(isOnVector, piece)
-
-        return isOnVector(piece) && blockingPieces.length == 0
-    }
-
-    const pieceAttackPatterns = {
-        "Q": (piece) =>
-            canAttack(piece, isInStraightLine) || canAttack(piece, isOnDiagonal),
-        "R": (piece) => canAttack(piece, isInStraightLine),
-        "B": (piece) => canAttack(piece, isOnDiagonal),
-        "N": (piece) => canKnightAttack(piece),
-        "K": () => false,
-        "P": (piece) => canPawnAttack(piece),
-        " ": () => false
-    }
-
-    const pieces = getPiecesFromBoard()
-
-    return pieces.filter((p) => pieceAttackPatterns[p.type](p)).length > 0
-};
+export const square = board => {
+  return board.replace(/\s+/g, '').split('')
+}
+export default board => {
+  const squares = board.split()
+  const kingIndex = squares.indexOf('K')
+  return false
+}
--- king_in_check.test.js
+++ king_in_check.test.js
@@ -1,239 +1,46 @@
 import cases from 'jest-in-case'
-import kingInCheck, {getPiecesFromBoard} from './king_in_check'
-import cloneDeep from 'lodash/cloneDeep'
+import kingInCheck, {square} from './king_in_check'
 
-const emptyBoard =
-    [
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '']
-    ]
+const board = `
+K_______
+________
+________
+________
+________
+________
+________
+________
+`;
 
-let board
+it(`returns a array of squares`, () => {
+  expect(Array.isArray(square(board))).toBe(true)
+  expect(square(board).length).toBe(64)
+  // expect(square(board).indexOf('K')).toBe(0)
+});
 
-beforeEach(() => {
-    board = cloneDeep(emptyBoard)
-})
-
-
-function convertBoard(board) {
-  board = board.trim()
-  board = board.replace(/ /g, '')
-  board = board.replace(/_/g, ' ')
-  board = board.split('\n')
-  for (let i = 0; i < 8; i++) {
-    board[i] = [...board[i]]
-  }
-  return board
-}
-
-cases(
-  "It can tell if a board is in check",
-  (args) => {
-    let board = convertBoard(args.board);
-    expect(kingInCheck(board)).toBe(args.check);
+cases('King in Check', args => {
+    expect(kingInCheck(args.board)).toBe(args.result)
   },
-  [
-    {
-      name: "returns false when attacking piece is blocked",
-      board: `
-     ________
-     ___Q____
-     ___P____
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name: "returns false when king is alone",
-      board: `
-     ________
-     ________
-     ________
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name: "returns true when king is in check from above in the same file",
-      board: `
-     ________
-     ________
-     ___Q____
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: true,
-    },
-    {
-      name:
-        "returns false when king is not in check from above in the same file",
-      board: `
-     ________
-     ________
-     ___P____
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name: "returns true when king is in check from NE",
-      board: `
-     ________
-     ________
-     __P_____
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: true,
-    },
-    {
-      name: "returns false when king is not in check from NE",
-      board: `
-     ________
-     ________
-     __N_____
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name: "returns true when king is in check from same rank",
-      board: `
-     ________
-     ________
-     ________
-     ___K_R__
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: true,
-    },
-    {
-      name: "returns true when king is in check from south diagonal",
-      board: `
-     ________
-     ________
-     ________
-     ___K____
-     ________
-     _____B__
-     ________
-     ________
-    `,
-      check: true,
-    },
-    {
-      name:
-        "returns false when king is not in check from pawn on north diagonal",
-      board: `
-     ________
-     _P______
-     ________
-     ___K____
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name:
-        `should return false when king is not in check because there is a piece in the way diagonally`,
-      board: `
-     ________
-     ________
-     ________
-     ___K____
-     ____R___
-     _____B__
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name:
-        `should return false when king is not in check because there is a piece in the way in the same rank`,
-      board: `
-     ________
-     ________
-     ________
-     ___K__PR
-     ________
-     ________
-     ________
-     ________
-    `,
-      check: false,
-    },
-    {
-      name:
-        `should return false when king is not in check because there is a piece in the way in the same file`,
-      board: `
-     ________
-     ________
-     ________
-     ___K____
-     ___P____
-     ________
-     ___R____
-     ________
-    `,
-      check: false,
-    },
-    {
-      name:
-        `should return true when king is in check by knight`,
-      board: `
-     ________
-     ________
-     ________
-     ___K____
-     ________
-     ____N___
-     ________
-     ________
-    `,
-      check: true,
-    }
+  [{
+    name: 'king alone is not in check',
+    board:
+      board,
+    result: false
+  },
+    // {
+    //   name: 'king can be checked from piece in straight line',
+    //   board:
+    //     `
+    //   ________
+    //   ___R____
+    //   ________
+    //   ___K____
+    //   ________
+    //   ________
+    //   ________
+    //   ________
+    //   `,
+    //   result: true
+    // }
   ]
 )
-
-function getPiece(type, rank, file) {
-    return {type: type, rank: rank, file: file}
-}
-
-const addPieceToBoard = (pieces) => {
-    pieces.forEach(piece => board[piece.rank][piece.file] = piece.type)
-}
