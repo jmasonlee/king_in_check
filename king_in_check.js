d1396dabc49ca5eeaa7f48ced11eca458fcf8364
Amaya Navarro <anavarro@atb.com>
2021-05-06 11:08:05 -0600

    Cycle2 V3

--- king_in_check.js
+++ king_in_check.js
@@ -1,8 +1,14 @@
 export const square = board => {
-  return board.replace(/\s+/g, '').split('')
+    return board.replace(/\s+/g, '').split('')
 }
 export default board => {
-  const squares = board.split()
-  const kingIndex = squares.indexOf('K')
-  return false
+    const squares = square(board)
+    const pieces = squares.map((s, index) => {
+        return {
+            type: s,
+            index
+        }
+    }).filter(s => s.type != '_')
+    const kingIndex = squares.indexOf('K')
+    return pieces.length > 1
 }
--- king_in_check.test.js
+++ king_in_check.test.js
@@ -15,8 +15,20 @@ ________
 it(`returns a array of squares`, () => {
   expect(Array.isArray(square(board))).toBe(true)
   expect(square(board).length).toBe(64)
-  // expect(square(board).indexOf('K')).toBe(0)
+  expect(square(board).indexOf('K')).toBe(0)
 });
+
+//Test case TODOs
+// A piece can't attack if it's blocked
+// A queen can attack on diagonal
+// A queen can attack on straight line
+// A bishop can attack on diagonal
+// A bishop can't attack on straight line
+// A rook can't attack on diagonal
+// A knight attacks in an L
+// A knight can't attack when not in an L
+// A pawn can attack from one north diagonal square away
+// A pawn can not attack from anywhere that's not one north diagonal
 
 cases('King in Check', args => {
     expect(kingInCheck(args.board)).toBe(args.result)
@@ -27,20 +39,35 @@ cases('King in Check', args => {
       board,
     result: false
   },
-    // {
-    //   name: 'king can be checked from piece in straight line',
-    //   board:
-    //     `
-    //   ________
-    //   ___R____
-    //   ________
-    //   ___K____
-    //   ________
-    //   ________
-    //   ________
-    //   ________
-    //   `,
-    //   result: true
-    // }
+    {
+      name: 'king can be checked from piece in straight line',
+      board:
+        `
+      ________
+      ___R____
+      ________
+      ___K____
+      ________
+      ________
+      ________
+      ________
+      `,
+      result: true
+    },
+    {
+      name: 'king can not be checked from piece in straight line if attacking piece is blocked',
+      board:
+          `
+      ________
+      ___R____
+      ___P____
+      ___K____
+      ________
+      ________
+      ________
+      ________
+      `,
+      result: false
+    }
   ]
 )
